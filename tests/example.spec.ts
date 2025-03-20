5c2a6d3d35735923fc5dcb49d0096d24f138a0e1
Mifras Gaffoor <81522853+mifrasgaffoor@users.noreply.github.com>
2025-03-20 15:37:39 +0530

    removed pre test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,20 +7,5 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
 
 
-
-test('logo is visible on the homepage', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const logo = page.locator('img[alt="Playwright logo"]');
-  await expect(logo).toBeVisible();
-});
